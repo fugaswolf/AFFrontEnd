b9823e55e74155c02a6efb7d7016911991085b0c
fu6as <alvi.istamalov@gmail.com>
2020-12-17 20:53:58 +0100

    added Product class + Service + show product list

---
+++ angular-frontend/src/app/common/product.ts
@@ -0,0 +1,17 @@
+export class Product {
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    active: boolean;
+    stock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+
+    constructor() {}
+
+
+
+
+}
