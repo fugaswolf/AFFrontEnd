22b3ccc8e2d6170b09dc0b1f98dbeb4e251657c0
fu6as <alvi.istamalov@gmail.com>
2020-12-18 15:46:46 +0100

    setup routes, show products by cat Id

--- angular-frontend/src/app/components/product-list/product-list.component.ts
+++ angular-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,16 +13,31 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(){
-    this.listProducts();
-
+    this.route.paramMap.subscribe(() =>{
+      this.listProducts();
+    });
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+    // is de Id param meegegeven?
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      // convert id string param to number (by using the + symbol)
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      // als er geen id meegegeven wordt dan.. standaard: category 1 wordt weergeven
+      this.currentCategoryId = 1;
+
+    }
+
+    // weergeef de producten met de juiste id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
--- angular-frontend/src/app/services/product.service.ts
+++ angular-frontend/src/app/services/product.service.ts
@@ -14,7 +14,10 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient ) { }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    //
+
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
